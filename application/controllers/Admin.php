3a0c980c862c2e01a2cb15df551c562b540e119e
loic <lroussel2703@gmail.com>
2018-11-23 17:38:36 +0100

    premier commit

---
+++ application/controllers/Admin.php
@@ -0,0 +1,14 @@
+<?php
+class Admin extends CI_Controller {
+//Controlleur principal du site
+public function __construct()
+    {
+        parent::__construct();   
+        $this->load->helper('form','url');       
+        $this->load->library('form_validation','session');        
+    }
+	public function index()
+	{
+		$this->load->view('site/index');
+	}
+}
---
+++ application/views/site/index.php
@@ -0,0 +1,147 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js"> <!--<![endif]-->
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="description" content="Aviato E-Commerce Template">
+  
+  <meta name="author" content="Themefisher.com">
+
+  <title>Lemon Interactive | Géolocalisation</title>
+
+  <!-- Mobile Specific Meta-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url() ?>assets/site/images/favicon.png" />
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/site/plugins/bootstrap/css/bootstrap.min.css">
+  <!-- Ionic Icon Css -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/site/plugins/Ionicons/css/ionicons.min.css">
+  <!-- animate.css -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/site/plugins/animate-css/animate.css">
+  <!-- Magnify Popup -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/site/plugins/magnific-popup/dist/magnific-popup.css">
+  <!-- Owl Carousel CSS -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/site/plugins/slick-carousel/slick/slick.css">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/site/plugins/slick-carousel/slick/slick-theme.css">
+  
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/site/css/style.css">
+
+</head>
+
+<body id="body">
+
+<!-- Header Start -->
+<header class="navigation">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<!-- header Nav Start -->
+				<nav class="navbar">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="https://www.lemon-interactive.fr/" target='blank'>
+								<img src="<?php echo base_url() ?>assets/site/images/logo.png" alt="Logo">
+							</a>
+						</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="<?php echo base_url() ?>">Acceuil</a></li>
+                <li><a href="<?php echo base_url() ?>">S'inscrire</a></li>								
+                <li><a href="<?php echo base_url() ?>">Contact</a></li>
+                <li><a href="<?php echo base_url() ?>">Administrateur</a></li>
+							</ul>
+							</div><!-- /.navbar-collapse -->
+							</div><!-- /.container-fluid -->
+						</nav>
+					</div>
+				</div>
+			</div>
+			</header><!-- header close -->
+
+<!-- Slider Start -->
+<section class="slider">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="block">
+					<h1 class="animated fadeInUp">Un site pour s'inscrire <br> Réalisé par BlueStier</h1>
+					<p class="animated fadeInUp">Et un back-office  pour récupérer les informations des internautes<br>  Ceci est un test proposé par Lemon-Interactive</p>
+					<a href="<?php echo base_url() ?>"  class="btn btn-main animated fadeInUp" >S'inscrire</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- footer Start -->
+<footer class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="footer-manu">
+					<ul>
+						<li><a href="https://www.lemon-interactive.fr/">Lemon-Interactive</a></li>
+						<li><a href="https://www.lemon-interactive.fr/vocation/">L'agence</a></li>
+						<li><a href="#">GitHub BlueStier</a></li>
+						<li><a href="#">Loïc Roussel qui est-ce ?</a></li>						
+					</ul>
+				</div>
+				<p class="copyright">Copyright 2018 &copy;Réalisé par BlueStier | Design par <a href="http://www.themefisher.com">themefisher.com</a>. All rights reserved.
+				</p>
+			</div>
+		</div>
+	</div>
+</footer>
+
+    <!-- 
+    Essential Scripts
+    =====================================-->
+    
+    <!-- <script src="js/jquery.counterup.js"></script> -->
+    
+    <!-- Main jQuery -->
+   
+    <script src="https://code.jquery.com/jquery-git.min.js"></script>
+    <!-- Bootstrap 3.1 -->
+    <script src="<?php echo base_url() ?>assets/site/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Owl Carousel -->
+    <script src="<?php echo base_url() ?>assets/site/plugins/slick-carousel/slick/slick.min.js"></script>
+    <!--  -->
+    <script src="<?php echo base_url() ?>assets/site/plugins/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
+    <!-- Mixit Up JS -->
+    <script src="<?php echo base_url() ?>assets/site/plugins/mixitup/dist/mixitup.min.js"></script>
+    <!-- <script src="plugins/count-down/jquery.lwtCountdown-1.0.js"></script> -->
+    <script src="<?php echo base_url() ?>assets/site/plugins/SyoTimer/build/jquery.syotimer.min.js"></script>
+
+
+    <!-- Form Validator -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.32/jquery.form.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.11.1/jquery.validate.min.js"></script>
+
+
+    
+    <!-- Google Map -->
+    <script src="http://localhost/Lemon-Interactive/assets/plugins/google-map/map.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkeLMlsiwzp6b3Gnaxd86lvakimwGA6UA&callback=initMap"></script>    
+
+    <script src="<?php echo base_url() ?>assets/site/js/script.js"></script>
+    
+
+
+
+  </body>
+  </html>
+   
